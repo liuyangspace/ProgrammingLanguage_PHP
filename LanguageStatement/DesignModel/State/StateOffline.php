a60509ce6dc8b78fcd241ccf3b36b74ccb8c56da
liuyangspace <2260597513@qq.com>
2016-12-30 11:51:57 +0800

    Changes not staged for commit:

--- LanguageStatement/DesignModel/State/StateOffline.php
+++ LanguageStatement/DesignModel/State/StateOffline.php
@@ -5,16 +5,12 @@
  * Date: 2016/12/30
  * Time: 11:23
  */
-
 namespace LanguageStatement\DesignModel\State;
-
 
 class StateOffline implements State
 {
-
     public function aboutMessage($message)
     {
         echo "offline now\n";
     }
-
 }
--- LanguageStatement/DesignModel/State/StateOnline.php
+++ LanguageStatement/DesignModel/State/StateOnline.php
@@ -5,16 +5,12 @@
  * Date: 2016/12/30
  * Time: 11:27
  */
-
 namespace LanguageStatement\DesignModel\State;
-
 
 class StateOnline implements State
 {
-
     public function aboutMessage($message)
     {
         echo "message : $message\n";
     }
-
 }
