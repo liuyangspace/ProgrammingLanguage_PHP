c2ee5d3eccda1506fef231bd9106f886c389ab2f
刘扬 <liuyang@huan.tv>
2018-06-15 09:42:17 +0800

    declare 注释调整

--- LanguageStatement/LanguageRegulate.php
+++ LanguageStatement/LanguageRegulate.php
@@ -3,8 +3,8 @@
  * php基础：php 调控
  * php statement：
  *  变量名由字母或者下划线开头，后面跟上任意数量的字母，数字，或者下划线。
- * 代码执行指令：
- *  declare结构：declare (directive)  statement
+ * declare 代码执行指令：
+ *  语法：declare (directive)  statement
  *  Tick（时钟周期）指令：
  *      在 declare 中的 directive 部分用 ticks=N 来指定，声明代码段中解释器每执行 N 条可计时的低级语句就会发生的事件
  *      每个 tick 中出现的事件是由 register_tick_function()/unregister_tick_function()来指定的。
@@ -13,10 +13,11 @@
  *      当和命名空间结合起来时 declare 的唯一合法语法是 declare(encoding='...').
  * Reference:
  *  http://php.net/manual/zh/langref.php
+ * Git: https://github.com/liuyangspace
+ * Date: 2016/12/12 10:32
  */
-
+declare(encoding = 'UTF-8'); // 指定脚本编码方式。
 namespace LanguageStatement;
-
 
 class LanguageRegulate
 {
@@ -44,11 +45,3 @@ class LanguageRegulate
     public static function get_required_files(){return get_required_files();}//别名 get_included_files()
 
 }
-
-/**
- * Created by PhpStorm.
- * User: liuyang
- * Git: https://github.com/liuyangspace
- * Date: 2016/12/12
- * Time: 10:32
- */
